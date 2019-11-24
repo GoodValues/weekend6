e80c37125b167087a325affdead24986caccae17
Adam Niewiadomski <adam.n@VENTIA.local>
2019-11-24 16:04:15 +0100

    LinkedList

--- src/listimpl/ListMain.java
+++ src/listimpl/ListMain.java
@@ -4,7 +4,8 @@ import java.util.Random;
 
 public class ListMain {
     public static void main(String[] args) {
-        IList<Integer> list = new ArrayList1<>();
+        // IList<Integer> list = new ArrayList1<>(); // w zależności obiekt, której klasy chcemy implementować
+        IList<Integer> list = new ArrayList2x<>();
         Random rand=new Random();
         int numbersCount=10;
         for (int i = 0; i < numbersCount; i++) {
